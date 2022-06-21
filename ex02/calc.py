6a330797b0286b77e0745d352ce744b29cbbeb9c
takahashi <c0a2107885@edu.teu.ac.jp>
2022-06-21 15:41:26 +0900

    3限までの基本的な電卓

--- ex02/calc.py
+++ ex02/calc.py
@@ -7,7 +7,7 @@ def button_click(event):
     if num == "=":
         eqn = entry.get()
         res = eval(eqn)
-        entry.delete()
+        entry.delete(0, tk.END)
         entry.insert(tk.END, res)
     else:
         ##tkm.showinfo("",f"{num}のボタンがクリックされました")
@@ -16,29 +16,27 @@ def button_click(event):
 #簡易的な電卓作ってみた！
 
 if __name__ == "__main__":
-    root = tk.Tk()
-    root.title("簡易的な電卓作ってみた！")
-    #root.geometry("300x500")
+   root = tk.Tk()
+   root.title("簡易的な電卓作ってみた！")
+   #root.geometry("300x500")
+   
+   entry = tk.Entry(root,justify="right", width=10, font=("Times New Roman",40))
+   entry.grid(row=0, column=0, columnspan=3)
+   
+   r, c = 1, 0 #行列
+   for i,num in enumerate([9, 8, 7, 6, 5, 4, 3, 2, 1, 0, "+", "="]):
+       btn = tk.Button(root, 
+                       text=f"{num}", 
+                       width=4, 
+                       height=2,
+                       font=("Times New Roman", 30)
+                      )
+       btn.bind("<1>", button_click)
+       btn.grid(row=r, column=c)
 
-
-    entry = tk.Entry(root,justify="right", width=10, font=("Times New Roman",40))
-    entry.grid(row=0, column=0, cloumnspan=3)
-
-    r, c = 1, 0 #行列
-    for i,num in enumerate([9, 8, 7, 6, 5, 4, 3, 2, 1, 0, "+"]):
-        btn = tk.Button(root, 
-                        text=f"{num}", 
-                        width=4, 
-                        height=2,
-                        font=("Times New Roman", 30)
-                        )
-        btn.bind("<1>", button_click)
-        btn.grid(row=r, column=c)
-
-        c += 1
-        if (i+1)%3 == 0:
-            r += 1
-            c = 0
-        
-
-    root.mainloop()
+       c += 1
+       if (i+1)%3 == 0:
+           r += 1
+           c = 0
+    
+root.mainloop()
