55f9dff7c90172db0856c7ab6ce1ecfc5148d1a2
takahashi <c0a2107885@edu.teu.ac.jp>
2022-07-05 14:33:51 +0900

    問４

--- ex04/dodge_bomb.py
+++ ex04/dodge_bomb.py
@@ -20,12 +20,20 @@ def main():
 
     while True:
         screen_sfc.blit(bgimg_sfc, bgimg_rct)
-        screen_sfc.blit(kkimg_sfc, kkimg_rct)
+        #screen_sfc.blit(kkimg_sfc, kkimg_rct)
         
         #問2
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 return
+        
+        #問4
+        key_states = pg.key.get_pressed()                   #辞書
+        if key_states[pg.K_UP]    == True: kkimg_rct.centery -=1         #y座標を-1
+        if key_states[pg.K_DOWN]  == True: kkimg_rct.centery +=1         #y座標を+1
+        if key_states[pg.K_LEFT]  == True: kkimg_rct.centerx -=1         #x座標を-1
+        if key_states[pg.K_RIGHT] == True: kkimg_rct.centerx +=1         #x座標を+1
+        screen_sfc.blit(kkimg_sfc, kkimg_rct)
 
         pg.display.update()  
         clock.tick(1000)
