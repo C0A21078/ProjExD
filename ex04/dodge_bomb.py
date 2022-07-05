d8aad33d30bb44a1568db53e2c096c72a4b56ddd
takahashi <c0a2107885@edu.teu.ac.jp>
2022-07-05 14:23:49 +0900

    問３

--- ex04/dodge_bomb.py
+++ ex04/dodge_bomb.py
@@ -4,20 +4,25 @@ import sys
 
 def main():
     clock = pg.time.Clock()
+    #問1
     pg.display.set_caption("逃げろ!こうかとん")
-    screen_sfc = pg.display.set_mode((1600, 900))   #Surface
-    screen_rct = screen_sfc.get_rect()              #Rect
-    bgimg_sfc  = pg.image.load("fig/pg_bg.jpg")     #Surface
-    bgimg_rct  = bgimg_sfc.get_rect()               #Rect
+    screen_sfc = pg.display.set_mode((1600, 900))          #Surface
+    screen_rct = screen_sfc.get_rect()                     #Rect
+    bgimg_sfc  = pg.image.load("fig/pg_bg.jpg")            #Surface
+    bgimg_rct  = bgimg_sfc.get_rect()                      #Rect
     screen_sfc.blit(bgimg_sfc, bgimg_rct)
 
-    #pg.display.update()                       
-    clock.tick(1)
+    #問3
+    kkimg_sfc = pg.image.load("fig/3.png")                 #Surface
+    kkimg_sfc = pg.transform.rotozoom(kkimg_sfc, 0, 2.0)   #Surface
+    kkimg_rct = kkimg_sfc.get_rect()                       #Rect
+    kkimg_rct.center = 900, 400
 
     while True:
         screen_sfc.blit(bgimg_sfc, bgimg_rct)
+        screen_sfc.blit(kkimg_sfc, kkimg_rct)
         
-        #練習2
+        #問2
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 return
